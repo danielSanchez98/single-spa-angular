92aceafa6b1415d5f697bfe4f5be378df8af714e
Daniel <daniales@bancolombia.com.co>
2022-08-09 12:50:49 -0500

    base

---
+++ test/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'test';
+
+  toggleDarkMode(evt:any) {
+    
+    if (evt.target.checked) {
+      document.body.classList.remove('is-light-mode');
+      document.body.classList.add('is-dark-mode');
+    } else {
+      document.body.classList.remove('is-dark-mode');
+      document.body.classList.add('is-light-mode');
+    }
+  }
+}
